d3e982b07cd96355160880473f0c4f09f171a53e
unknown <yogeshd@yogeshd-laptop.cybage.com>
2013-05-10 15:24:28 +0530

    issue53 related changes

--- data/src/main/java/com/yogi/data/connection/ConnectionFactory.java
+++ data/src/main/java/com/yogi/data/connection/ConnectionFactory.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 // yess  yogi bhai
+//issue 53 commit
 public class ConnectionFactory {
 
 	private static SqlSessionFactory sqlMapper;
